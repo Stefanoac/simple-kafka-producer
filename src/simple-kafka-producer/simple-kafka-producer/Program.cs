de5177724bbda9c425b25ad27d2eea9e538bea8f
Stéfano Caberlon <stefanocaberlon5@gmail.com>
2021-05-13 13:51:39 -0300

    add kafka productor service

--- src/simple-kafka-producer/simple-kafka-producer/Program.cs
+++ src/simple-kafka-producer/simple-kafka-producer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Confluent.Kafka;
+using simple_kafka_producer.Services;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,27 +10,7 @@ namespace simple_kafka_producer
     {
         public static async Task Main()
         {
-            var config = new ProducerConfig { BootstrapServers = "127.0.0.1:9092" };
-
-            using (var p = new ProducerBuilder<Null, string>(config).Build())
-            {
-                try
-                {
-                    var count = 0;
-                    while (true)
-                    {
-                        var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = $"test: {count++}" });
-
-                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
-
-                        Thread.Sleep(2000);
-                    }
-                }
-                catch (ProduceException<Null, string> e)
-                {
-                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
-                }
-            }
+            await KafkaProductor.SendMessage();
         }
     }
 }
---
+++ src/simple-kafka-producer/simple-kafka-producer/Services/KafkaProductor.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace simple_kafka_producer.Services
+{
+    public class KafkaProductor
+    {
+        public const String HOST = "127.0.0.1:9092";
+        public const String KAFKA_TOPIC_NAME = "test-topic";
+        public const int TIME_DELAY = 2000;
+
+        public static async Task SendMessage()
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            var config = new ProducerConfig { BootstrapServers = HOST };
+
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var count = 0;
+                    while (true)
+                    {
+                        var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = $"test: {count++}" });
+
+                        Log.Logger.Information($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
+                        Thread.Sleep(TIME_DELAY);
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
